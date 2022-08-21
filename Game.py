c77a44d6a138b133277646b8b516f9524fb9fbbd
Acidicts <111660544+Acidicts@users.noreply.github.com>
2022-08-21 16:22:41 +0100

    Weird Upload Glitch
    I have placed the real code

--- Game.py
+++ Game.py
@@ -1,2 +1,15 @@
-def play():
-    print("olah")
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+app = Ursina()
+
+ground = Entity(
+    model = "plane",
+    texture = "grass",
+    collider = "mesh",
+    scale = (1000, 1, 1000)
+)
+
+player = FirstPersonController()
+
+app.run()
